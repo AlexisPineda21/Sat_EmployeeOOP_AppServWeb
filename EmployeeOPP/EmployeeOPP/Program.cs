0e945aee74707be2dc47eb6aa42b16cfc79dc1bb
Alexis <oschan2004@gmail.com>
2023-09-13 22:59:27 -0500

    Project avance day finished

--- EmployeeOPP/EmployeeOPP/Program.cs
+++ EmployeeOPP/EmployeeOPP/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using EmployeeOPP.Classes;
+
+try
+{
+    Console.WriteLine("OOP APPLICATION");
+    Console.WriteLine("---------------");
+
+    int day, month, year;
+
+    Console.Write("Ingresar el día: ");
+    day = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Ingresar el mes: ");
+    month = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Ingresar el año: ");
+    year = Convert.ToInt32(Console.ReadLine());
+
+    //Inicialización clase Date
+    Date dateObject = new Date(day, month, year);
+    Console.WriteLine("\n");
+    Console.WriteLine(dateObject.ToString());
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+

9c233fcd0da4e86ed7798d563978e9eaa30f3135
Alexis <oschan2004@gmail.com>
2023-09-16 20:42:49 -0500

    New classes HourlyEmployee and BaseCommissionEmployee created

---
+++ EmployeeOPP/EmployeeOPP/Classes/BaseCommissionEmployee.cs
@@ -0,0 +1,33 @@
+﻿namespace EmployeeOPP.Classes
+{
+    public class BaseCommissionEmployee : CommissionEmployee
+    {
+
+        #region Properties
+
+        public decimal Base { get; set; }
+
+
+        #endregion
+
+        #region Methods
+
+        public BaseCommissionEmployee() 
+        { 
+        }
+
+        public override decimal GetValueToPay()
+        {
+            return Base + ((CommissionPercentaje / 100) * Sales);
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}" +
+                $"Salario devengado por comisiones + base es: {Base + ((CommissionPercentaje / 100) * Sales):C2} \n\t ";
+        }
+
+        #endregion
+
+    }
+}
--- EmployeeOPP/EmployeeOPP/Program.cs
+++ EmployeeOPP/EmployeeOPP/Program.cs
@@ -11,6 +11,11 @@ try
     string firstName;
     string lastName;
     decimal salary;
+    decimal CommissionPercentaje;
+    decimal HourValue;
+    decimal Sales;
+    decimal BaseCommission;
+    int Hours;
 
     Console.Write("Ingresar el día: ");
     day = Convert.ToInt32(Console.ReadLine());
@@ -51,6 +56,73 @@ try
 
 
     Console.WriteLine(salaryEmployee);
+
+
+    Console.Write("Ingresar comision (Porcentaje): ");
+    CommissionPercentaje = Decimal.Parse(Console.ReadLine());
+
+    Console.Write("Ingresar el valor de ventas: ");
+    Sales = Decimal.Parse(Console.ReadLine());
+
+    CommissionEmployee commissionEmployee = new CommissionEmployee()
+    {
+        Id = 123456,
+        FirstName = firstName,
+        LastName = lastName,
+        BirthDate = dateObject,
+        HiringDate = dateObject,
+        IsActive = true,
+        Sales = Sales,
+        CommissionPercentaje = CommissionPercentaje
+    };
+
+    Console.WriteLine(commissionEmployee);
+
+    Console.WriteLine("\n");
+
+
+    Console.Write("Ingresar comision base del empleado: ");
+    BaseCommission = Decimal.Parse(Console.ReadLine());
+
+    BaseCommissionEmployee baseCommissionEmployee = new BaseCommissionEmployee()
+    {
+        Id = 123456,
+        FirstName = firstName,
+        LastName = lastName,
+        BirthDate = dateObject,
+        HiringDate = dateObject,
+        IsActive = true,
+        Sales = Sales,
+        CommissionPercentaje = CommissionPercentaje,
+        Base = BaseCommission,
+    };
+
+    Console.WriteLine(baseCommissionEmployee);
+
+    Console.WriteLine("\n");
+
+
+    Console.Write("Ingresar número de horas: ");
+    Hours = Int32.Parse(Console.ReadLine());
+
+    Console.Write("Ingresar valor por hora: ");
+    HourValue = Decimal.Parse(Console.ReadLine());
+
+    HourlyEmployee hourlyEmployee = new HourlyEmployee()
+    {
+        Id = 123456,
+        FirstName = firstName,
+        LastName = lastName,
+        BirthDate = dateObject,
+        HiringDate = dateObject,
+        IsActive = true,
+        HourValue = HourValue,
+        Hours = Hours,
+    };
+
+    Console.WriteLine(hourlyEmployee);
+
+    Console.WriteLine("\n");
 
 }
 catch (Exception ex)
